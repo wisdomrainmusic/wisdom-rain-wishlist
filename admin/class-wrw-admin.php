629ea449c6adf8fe608b84169fd32d676307565a
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2025-11-30 14:41:13 +0300

    fix: Add missing WRW_Admin::enqueue_admin_assets method to prevent fatal error

--- admin/class-wrw-admin.php
+++ admin/class-wrw-admin.php
@@ -1,6 +1,19 @@
 <?php
-
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class WRW_Admin {
+
+    /**
+     * Load admin assets
+     */
+    public static function enqueue_admin_assets() {
+        // Admin CSS/JS will be added later
+    }
+
+    /**
+     * Future: Admin menu
+     */
+    public static function register_menu() {
+        // Settings page placeholder
+    }
 }
